72e1caa5eaaf91259b8f37ea55cea5da38a0c3cf
salusn <salu.s@lightrains.com>
2017-07-31 18:00:11 +0530

    Displayed description

--- src/components/Blogdetails.js
+++ src/components/Blogdetails.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Blogdetails = (props) => {
+	console.log(props)
 	
   return (
       <div>
-       {props.name}
+       {props.description}
       </div>
     );
 }
